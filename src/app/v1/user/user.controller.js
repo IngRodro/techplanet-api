dba291d449c850dff7c02c8c84684bb469a21391
Ing Rodro <elr0dr0@outlook.com>
2024-08-02 21:44:04 -0600

    Revert

--- src/app/v1/user/user.controller.js
+++ src/app/v1/user/user.controller.js
@@ -30,7 +30,6 @@ export const signIn = async (req, res) => {
   const tokenOptions = {
     httpOnly: true,
     secure: false,
-    domain: 'https://techplanet-webapp.vercel.app'
   };
 
   return res.cookie('token', token, tokenOptions).json({
